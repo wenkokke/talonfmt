65c6210358e8d7d62f5981969f7c4eee421a4902
Wen Kokke <wenkokke@users.noreply.github.com>
2022-07-30 18:30:52 +0100

    Reenabled alignment tests and added idempotency tests

--- test/test_golden.py
+++ test/test_golden.py
@@ -4,29 +4,39 @@ import talonfmt
 
 @pytest.mark.golden_test("golden/default/*.yml")
 def test_golden_default(golden):
-    output = talonfmt.talonfmt(
-        contents=golden["input"]
+    output1 = talonfmt.talonfmt(contents=golden["input"])
+    assert output1 == golden.out["output"]
+    output2 = talonfmt.talonfmt(contents=output1)
+    assert output2 == output1, f"Default formatting is not idempodent"
+
+
+@pytest.mark.golden_test("golden/align/dynamic/*.yml")
+def test_golden_align_dynamic(golden):
+    output1 = talonfmt.talonfmt(
+        contents=golden["input"], align_match_context=True, align_short_commands=True
     )
-    assert output == golden.out["output"]
+    assert output1 == golden.out["output"]
+    output2 = talonfmt.talonfmt(
+        contents=output1, align_match_context=True, align_short_commands=True
+    )
+    assert output2 == output1, f"Dynamic alignment is not idempodent"
 
 
-# @pytest.mark.golden_test("golden/align/dynamic/*.yml")
-# def test_golden_align_dynamic(golden):
-#     output = talonfmt.talonfmt(
-#         contents=golden["input"],
-#         align_match_context=True,
-#         align_short_commands=True
-#     )
-#     assert output == golden.out["output"]
-
-
-# @pytest.mark.golden_test("golden/align/fixed32/*.yml")
-# def test_golden_align_fixed32(golden):
-#     output = talonfmt.talonfmt(
-#         contents=golden["input"],
-#         align_match_context=True,
-#         align_match_context_at=32,
-#         align_short_commands=True,
-#         align_short_commands_at=32,
-#     )
-#     assert output == golden.out["output"]
+@pytest.mark.golden_test("golden/align/fixed32/*.yml")
+def test_golden_align_fixed32(golden):
+    output1 = talonfmt.talonfmt(
+        contents=golden["input"],
+        align_match_context=True,
+        align_match_context_at=32,
+        align_short_commands=True,
+        align_short_commands_at=32,
+    )
+    assert output1 == golden.out["output"]
+    output2 = talonfmt.talonfmt(
+        contents=output1,
+        align_match_context=True,
+        align_match_context_at=32,
+        align_short_commands=True,
+        align_short_commands_at=32,
+    )
+    assert output2 == output1, f"Fixed alignment is not idempodent"
