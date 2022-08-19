8be2f14843a16dd0e32810f84285b36e0e009261
Wen Kokke <wenkokke@users.noreply.github.com>
2022-08-19 14:18:37 +0100

    Deprecate idempotency tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,25 +3,11 @@ import typing
 
 import pytest
 
+pytest.register_assert_rewrite("talonfmt")
+pytest.register_assert_rewrite("talonfmt.main")
+pytest.register_assert_rewrite("talonfmt.formatter")
+
 import talonfmt
-
-
-def node_dict_simplify(node_dict: dict[str, typing.Any]) -> dict[str, typing.Any]:
-    if len(node_dict) > 4:
-        del node_dict["text"]
-
-    del node_dict["start_position"]
-    del node_dict["end_position"]
-
-    for key in node_dict.keys():
-        if isinstance(node_dict[key], dict):
-            node_dict_simplify(node_dict[key])
-        if isinstance(node_dict[key], list):
-            for val in node_dict[key]:
-                if isinstance(val, dict):
-                    node_dict_simplify(val)
-
-    return node_dict
 
 
 def golden_path(golden) -> str:
